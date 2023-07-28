b9c7213a42238f04e86cd78561bfa6039b5efdf8
Cee-Jay Malacas <ceejaymalacas17@gmail.com>
2023-07-28 18:45:54 +0800

    relationship added

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Listing extends Model
 {
@@ -23,5 +24,10 @@ class Listing extends Model
             ->orWhere('description', 'like', '%'.request('search').'%')
             ->orWhere('tags', 'like', '%'.request('search').'%');
         }
+    }
+
+    // Relationship to User
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
